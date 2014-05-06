d01de4ed29506005ba01e3edd6515925af2eb4b5
Chris Brown <xoebus@xoeb.us>
2014-05-06 04:27:30 +0100

    vendor dependencies

---
+++ Godeps/_workspace/src/github.com/xoebus/tracker/client.go
@@ -0,0 +1,82 @@
+package tracker
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var DefaultURL = "https://www.pivotaltracker.com"
+
+type Client struct {
+	client *http.Client
+	token  string
+}
+
+type Me struct {
+	Username string `json:"username"`
+}
+
+func NewClient(token string) *Client {
+	return &Client{
+		client: &http.Client{},
+		token:  token,
+	}
+}
+
+func (c Client) Me() (Me, error) {
+	var me Me
+
+	request, err := c.createRequest("/services/v5/me")
+	if err != nil {
+		return me, err
+	}
+
+	response, err := c.sendRequest(request)
+	if err != nil {
+		return me, err
+	}
+	defer response.Body.Close()
+
+	if err := c.decodeResponse(response, &me); err != nil {
+		return me, err
+	}
+
+	return me, nil
+}
+
+func (c Client) createRequest(path string) (*http.Request, error) {
+	request, err := http.NewRequest("GET", DefaultURL+path, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %s", err)
+	}
+	request.Header.Add("X-TrackerToken", c.token)
+
+	return request, nil
+}
+
+func (c Client) sendRequest(request *http.Request) (*http.Response, error) {
+	response, err := c.client.Do(request)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make request: %s", err)
+	}
+
+	if response.StatusCode == http.StatusUnauthorized {
+		return nil, errors.New("invalid token")
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("request failed (%d)", response.StatusCode)
+	}
+
+	return response, nil
+}
+
+func (c Client) decodeResponse(response *http.Response, object interface{}) error {
+	if err := json.NewDecoder(response.Body).Decode(object); err != nil {
+		return fmt.Errorf("invalid json response: %s", err)
+	}
+
+	return nil
+}
---
+++ Godeps/_workspace/src/github.com/xoebus/tracker/client_test.go
@@ -0,0 +1,119 @@
+package tracker_test
+
+import (
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/onsi/gomega/ghttp"
+
+	"github.com/xoebus/tracker"
+)
+
+func Fixture(filename string) string {
+	path := filepath.Join("fixtures", filename)
+	contents, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(contents)
+}
+
+var _ = Describe("Tracker Client", func() {
+	var (
+		server *ghttp.Server
+	)
+
+	BeforeEach(func() {
+		server = ghttp.NewServer()
+		tracker.DefaultURL = server.URL()
+	})
+
+	AfterEach(func() {
+		server.Close()
+	})
+
+	Describe("getting information about the current user", func() {
+		var statusCode int
+
+		It("works if everything goes to plan", func() {
+			statusCode = http.StatusOK
+			headers := http.Header{
+				"X-TrackerToken": {"api-token"},
+			}
+
+			server.AppendHandlers(ghttp.CombineHandlers(
+				ghttp.VerifyRequest("GET", "/services/v5/me"),
+				ghttp.VerifyHeader(headers),
+
+				ghttp.RespondWith(statusCode, Fixture("me.json")),
+			))
+
+			client := tracker.NewClient("api-token")
+			me, err := client.Me()
+
+			Ω(err).ToNot(HaveOccurred())
+			Ω(me.Username).To(Equal("vader"))
+		})
+
+		It("returns an error if the response is not successful", func() {
+			statusCode = http.StatusInternalServerError
+
+			server.AppendHandlers(ghttp.CombineHandlers(
+				ghttp.RespondWith(statusCode, ""),
+			))
+
+			client := tracker.NewClient("api-token")
+			_, err := client.Me()
+			Ω(err).To(MatchError("request failed (500)"))
+		})
+
+		It("returns a helpful error if the token is invalid", func() {
+			statusCode = http.StatusUnauthorized
+
+			server.AppendHandlers(ghttp.CombineHandlers(
+				ghttp.RespondWith(statusCode, ""),
+			))
+
+			client := tracker.NewClient("api-token")
+			_, err := client.Me()
+			Ω(err).To(MatchError("invalid token"))
+		})
+
+		It("returns an error if the request fails", func() {
+			server.Close()
+
+			client := tracker.NewClient("api-token")
+			_, err := client.Me()
+
+			Ω(err).To(HaveOccurred())
+			Ω(err.Error()).To(MatchRegexp("failed to make request"))
+			server = ghttp.NewServer()
+		})
+
+		It("returns an error if the request can't be created", func() {
+			tracker.DefaultURL = "aaaaa)#Q&%*(*"
+
+			client := tracker.NewClient("api-token")
+			_, err := client.Me()
+
+			Ω(err).To(HaveOccurred())
+			Ω(err.Error()).To(MatchRegexp("failed to create request"))
+		})
+
+		It("returns an error if the response JSON is broken", func() {
+			server.AppendHandlers(ghttp.CombineHandlers(
+				ghttp.RespondWith(http.StatusOK, `{"`),
+			))
+
+			client := tracker.NewClient("api-token")
+			_, err := client.Me()
+
+			Ω(err).To(HaveOccurred())
+			Ω(err.Error()).To(MatchRegexp("invalid json response"))
+		})
+	})
+})
